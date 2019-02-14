fe2bef713735b648a6e36874284ff22e21747317
devbruce <bruce93k@gmail.com>
2019-02-14 12:40:50 +0900

    Add basic table at index

--- app/config/views.py
+++ app/config/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from posts.models import Post
 
 
 def index(request):
-    context = {}
+    posts = Post.objects.all()
+    context = {
+        'posts': posts,
+    }
     return render(request, 'index.html', context)
--- app/posts/views.py
+++ app/posts/views.py
@@ -3,4 +3,3 @@ from django.shortcuts import render
 
 def post_detail(request, post_pk):
     pass
-
